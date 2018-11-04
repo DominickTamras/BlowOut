5b29a89f782897ae453f5014f18cf65134c132f4
RCLobster <44736025+RCLobster@users.noreply.github.com>
2018-11-03 17:23:36 -0700

    Add files via upload

---
+++ Update Folder/Assets/Code/BlockFall.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockFall : MonoBehaviour 
+{
+    public GameObject[] Cubes7;
+
+	// Use this for initialization
+	void Start () 
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		
+	}
+
+    public void OnTriggerEnter(Collider col)
+    {
+       if(Cubes7[0] && col.gameObject.tag == "Missle")
+        {
+            Destroy(Cubes7[0].gameObject);
+            print("destroyed");
+        }
+        if (Cubes7[6] && col.gameObject.tag == "Missle")
+        {
+            Destroy(Cubes7[6].gameObject);
+            print("destroyed");
+        }
+
+    }
+}
